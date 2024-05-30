1a5e7a51acec83b43a9bc52ff83fa106bab890e3
Elizaveta Chervonnaya <ntropy@bk.ru>
2024-05-30 14:01:38 +0300

    add ConnectionManager

---
+++ src/main/java/chervonnaya/util/ConnectionManager.java
@@ -0,0 +1,47 @@
+package chervonnaya.util;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+import java.io.IOException;
+
+public class ConnectionManager {
+    private static HikariDataSource dataSource;
+
+    static {
+        try (InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream("db.properties")) {
+            Properties properties = new Properties();
+            if (input == null) {
+                throw new IOException("Unable to find db.properties");
+            }
+            properties.load(input);
+
+            HikariConfig config = new HikariConfig();
+            config.setJdbcUrl(properties.getProperty("db.url"));
+            config.setUsername(properties.getProperty("db.username"));
+            config.setPassword(properties.getProperty("db.password"));
+            config.setMaximumPoolSize(Integer.parseInt(properties.getProperty("db.maximumPoolSize")));
+            config.setConnectionTimeout(Long.parseLong(properties.getProperty("db.connectionTimeout")));
+
+            dataSource = new HikariDataSource(config);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError("Initialization failed: " + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    public static void closeDataSource() {
+        if (dataSource != null && !dataSource.isClosed()) {
+            dataSource.close();
+        }
+    }
+}
+
